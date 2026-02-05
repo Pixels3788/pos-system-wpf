f8ab9f68f16c70acf5eb27d6ac0bc4697b16eebb
pixels3788 <pixels378@gmail.com>
2026-02-05 05:03:03 -0600

    Finished OrderService tests

--- PointOfSaleTests/OrderServiceTests.cs
+++ PointOfSaleTests/OrderServiceTests.cs
@@ -54,6 +54,8 @@ namespace PointOfSaleTests
             result.Should().NotBeNull();
         }
 
+        
+
         [Fact]
         public void DeleteOrder_ShouldReturnNull()
         {
@@ -83,6 +85,116 @@ namespace PointOfSaleTests
             result.Should().BeNull();
         }
 
+        [Theory]
+        [InlineData(13, 25)]
+        [InlineData(45, 55)]
+        public void UpdateOrderLineItemQuantity_ShouldReturnUpdatedItem(int quantity, int updatedQuantity)
+        {
+            var newMenuItem = _menuService.CreateMenuItem("Sosa", 250.53m, "rapper");
+            newMenuItem.Should().NotBeNull();
+
+            var newOrder = _orderService.CreateNewOrder();
+            newOrder.Should().NotBeNull();
+
+            var newLineItem = _orderService.CreateOrderLineItem(newMenuItem, newOrder, quantity);
+            newLineItem.Should().NotBeNull();
+
+            var result = _orderService.UpdateOrderLineItemQuantity(newLineItem.LineItemId, updatedQuantity);
+            result.Should().NotBeNull();
+            result.Quantity.Should().Be(updatedQuantity);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-1111)]
+        [InlineData(0)]
+        public void UpdateOrderLineItemQuantity_ShouldReturnNull(int updatedQuantity)
+        {
+            var newMenuItem = _menuService.CreateMenuItem("Kendrick", 1250.55m, "rapper");
+            newMenuItem.Should().NotBeNull();
+
+            var newOrder = _orderService.CreateNewOrder();
+            newOrder.Should().NotBeNull();
+
+            var newLineItem = _orderService.CreateOrderLineItem(newMenuItem, newOrder, 10);
+            newLineItem.Should().NotBeNull();
+
+            var result = _orderService.UpdateOrderLineItemQuantity(newLineItem.LineItemId, updatedQuantity);
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void FinalizeOrder_ShouldReturnFinalizedOrder()
+        {
+            var newOrder = _orderService.CreateNewOrder();
+
+            var result = _orderService.FinalizeOrder(newOrder.OrderId);
+
+            result.Should().NotBeNull();
+            result.IsFinalized.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GetLineItemsByOrder_ShouldReturnListOfLineItems()
+        {
+            var firstNewMenuItem = _menuService.CreateMenuItem("Test123", 2.57m, "testing");
+            var secondNewMenuItem = _menuService.CreateMenuItem("Testin456", 4.57m, "testing");
+
+            var newOrder = _orderService.CreateNewOrder();
+
+            var firstLineItem = _orderService.CreateOrderLineItem(firstNewMenuItem, newOrder, 3);
+            var secondLineItem = _orderService.CreateOrderLineItem(secondNewMenuItem, newOrder, 2);
+
+            var result = _orderService.GetOrderLineItemsByOrder(newOrder.OrderId);
+
+            result.Select(i => i.MenuItemId).Should().Contain(new[] { firstNewMenuItem.ItemId, secondNewMenuItem.ItemId });
+            result.Select(i => i.LineItemId).Should().Contain(new[] { firstLineItem.LineItemId, secondLineItem.LineItemId });
+            result.Select(i => i.OrderId).Should().Contain(new[] { newOrder.OrderId });
+        }
+
+        [Fact]
+        public void GetOpenOrders_ShouldReturnOpenOrders()
+        {
+            var firstOrder = _orderService.CreateNewOrder();
+            firstOrder.Should().NotBeNull();
+            var secondOrder = _orderService.CreateNewOrder();
+            secondOrder.Should().NotBeNull();
+            var thirdOrder = _orderService.CreateNewOrder();
+            thirdOrder.Should().NotBeNull();    
+
+            firstOrder = _orderService.FinalizeOrder(firstOrder.OrderId);
+            firstOrder.Should().NotBeNull();
+
+            var result = _orderService.GetOpenOrders();
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2);
+            result.Select(i => i.OrderId).Should().Contain(new[] { secondOrder.OrderId, thirdOrder.OrderId });
+
+          
+        }
+
+        [Fact]
+        public void GetFinalizedOrders_ShouldReturnOpenOrders()
+        {
+            var firstOrder = _orderService.CreateNewOrder();
+            firstOrder.Should().NotBeNull();
+
+            var secondOrder = _orderService.CreateNewOrder();
+            secondOrder.Should().NotBeNull();
+
+            var thirdOrder = _orderService.CreateNewOrder();
+            thirdOrder.Should().NotBeNull();
+
+            firstOrder = _orderService.FinalizeOrder(firstOrder.OrderId);
+            firstOrder.Should().NotBeNull();
+
+            secondOrder = _orderService.FinalizeOrder(secondOrder.OrderId);
+
+            var result = _orderService.GetFinalizedOrders();
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2);
+            result.Select(i => i.OrderId).Should().Contain(new[] { firstOrder.OrderId, secondOrder.OrderId });
+        }
 
 
     }
